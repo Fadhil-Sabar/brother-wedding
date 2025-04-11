7d899af984fc554c02c8ad52f4b1ca216ff1b34e
Fadhil Andriawan <fadhil@ilcs.co.id>
2025-04-11 20:02:53 +0700

    [FIX] add @vercel/analytics for improved tracking and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -49,7 +50,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
